27bb9da89ff081212714d59de70a8f8ab524e891
aquashdw <aquashdw@gmail.com>
2021-08-02 16:12:17 +0900

    basic endpoints for account-transaction service

--- account-transactions/src/app.controller.ts
+++ account-transactions/src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  private readonly logger = new Logger(AppController.name);
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/health')
+  healthCheck() {
+    this.logger.log('TODO health check');
+  }
+
+  @Post('/request-reservation')
+  requestReservation() {
+    this.logger.log('TODO request reservation');
   }
 }
