e216b4b2ece57c3b627f47f95c16cb68bc42588a
aquashdw <aquashdw@gmail.com>
2021-08-03 15:21:46 +0900

    remove get-hello from account-tranactions

--- account-transactions/src/app.controller.ts
+++ account-transactions/src/app.controller.ts
@@ -6,11 +6,6 @@ export class AppController {
   private readonly logger = new Logger(AppController.name);
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
   @Get('/health')
   healthCheck() {
     this.logger.log('TODO health check');
