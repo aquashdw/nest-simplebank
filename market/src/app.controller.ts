79708755d83d216dd10b61bc4c43aa0c606a200e
aquashdw <aquashdw@gmail.com>
2021-08-03 16:25:21 +0900

    rename function name

--- market/src/app.controller.ts
+++ market/src/app.controller.ts
@@ -8,7 +8,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('health')
-  getHello() {
+  healthCheck() {
     this.logger.log('TODO health check');
   }
 
