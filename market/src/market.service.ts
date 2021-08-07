045c890fa5a319d1d3ebbc7c372882eab896b2ef
aquashdw <aquashdw@gmail.com>
2021-08-07 09:02:44 +0900

    add functions (market)

--- market/src/market.service.ts
+++ market/src/market.service.ts
@@ -6,6 +6,10 @@ export class MarketService {
   private readonly logger = new Logger(MarketService.name);
   constructor(private readonly amqpConnection: AmqpConnection) {}
 
+  placeOrderToMarket(dto: any) {
+    // TODO
+  }
+
   async publishOrderPlaced(dto: any) {
     // TODO await this.amqpConnection.publish('nest.topic.exchange', routingKey, JSON.stringify(dto));
   }
